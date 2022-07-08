19b9a5f0a612377c9b9765525e6e6f144fb1459d
VP-developer <6494878+vfarago@users.noreply.github.com>
2022-07-08 11:26:25 +0900

    Kevin_0708
    신버전 업

---
+++ Assets/AssetBundleBuilder.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleBuilder : MonoBehaviour
+{
+    public class CreateAssetBundles
+    {
+        [MenuItem("Additional/Build AssetBundles")]
+        static void BuildAllAssetBundles()
+        {
+            BuildPipeline.BuildAssetBundles("Assets/BuildFiles", BuildAssetBundleOptions.None, BuildTarget.Android);
+        }
+    }
+}
+#endif
